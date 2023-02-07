12cb04d42bbd2a11e64896d14777fcf6305885b2
Rihards Priedolins <rihards.priedolins@gmail.com>
2023-02-07 18:47:15 +0200

    in progress

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        U-6:
+//        Izveidot vienkāršu chatbot funkcionalitāti ar lietotāja ievadi un If/else./
+//        Temats: Programmēšanas skola.
+//        Funkcijas: Spēj atbildēt uz 3 jautājumiem.
+//
+//                Piem:
+//        Caur Scanner paņemam lietotāja ievadi kā jautājumu un balstoties uz jautājumu - tiek sniegta atbilde ar - System.out.println()
+//
+//
+//        Iesniegt kā linku uz github repo.
+//
+//        Termiņš: 09.02.2023, plkst. 18:00!
+
+
+
+    }
+}
