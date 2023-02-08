9a571c48155dc501e4be53ed6aa9809799c0297c
Rihards Priedolins <rihards.priedolins@gmail.com>
2023-02-08 18:03:34 +0200

    finish

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,50 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-//        U-6:
-//        Izveidot vienkāršu chatbot funkcionalitāti ar lietotāja ievadi un If/else./
-//        Temats: Programmēšanas skola.
-//        Funkcijas: Spēj atbildēt uz 3 jautājumiem.
-//
-//                Piem:
-//        Caur Scanner paņemam lietotāja ievadi kā jautājumu un balstoties uz jautājumu - tiek sniegta atbilde ar - System.out.println()
-//
-//
-//        Iesniegt kā linku uz github repo.
-//
-//        Termiņš: 09.02.2023, plkst. 18:00!
+        Scanner input = new Scanner(System.in);
 
+
+
+        //1.jautājums
+
+        System.out.println("1. Jautājums: Kurā mēnesī sākās RCS programmēšanas kursi?");
+
+        String atbilde1 = input.nextLine();
+
+        if (atbilde1.equalsIgnoreCase("janvārī")) {
+            System.out.println("Precīzi!");
+
+        } else {
+            System.out.println("Nē "+ atbilde1 + " nebūs pareizi! Atbilde ir Janvārī.");
+        }
+
+        //2.jautājums
+
+        System.out.println("2. Jautājums: Kādu programmēšanas valodu Tu patreiz mācies?");
+
+        String atbilde2 = input.nextLine();
+
+        if (atbilde2.equalsIgnoreCase("java") ) {
+            System.out.println("Precīzi!");
+        } else {
+            System.out.println("Kļūda, " + atbilde2 + " nebūs īstā! Tu patreiz mācies Java.");
+        }
+
+        //3.jautājums
+
+        System.out.println("3. Jautājums: Vai Java ir OOP (Object-oriented programming) valoda ?");
+
+        String atbilde3 = input.nextLine();
+
+        if (atbilde3.equalsIgnoreCase("jā") ) {
+            System.out.println("Nu gan Tu esi gudrinieks!");
+        } else {
+            System.out.println("Kļūdies! Jo atbilde ir Jā, Java ir OOP valoda.");
+        }
+
+        System.out.println("Paldies par sarunu!");
 
 
     }
